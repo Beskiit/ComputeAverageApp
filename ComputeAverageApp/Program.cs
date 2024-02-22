1683b605b72466d19393c749512be0d4efe3a431
gamil.293092 <gamil.293092@LAB-A-PC08>
2024-02-23 07:47:14 +0800

    Add project files.

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            double totalGrade = 0;
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            while(true)
+            {
+                int grade = Convert.ToInt32(Console.ReadLine());
+                totalGrade += grade;
+                x++;
+                if(x == 5)
+                {
+                    break;
+                }
+            }
+
+            double average = totalGrade / 5;
+            Console.WriteLine("The average is " + average + " and round off to " + Math.Round(average));
+        }
+    }
+}
